79f7172f095f8eacf090e8b48762ac45d4a419f9
Robson Paiva <paivawork@hotmail.com>
2019-12-28 14:40:36 -0300

    Adicionada classe Conexao

---
+++ Model/Conexao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Conexao
+    {
+        public static string conexao = "Data Source = PAIVA-PC/SQLEXPRESS; Initial Catalog = LOCACAO; User = sa; Password = 123";
+    }
+}
