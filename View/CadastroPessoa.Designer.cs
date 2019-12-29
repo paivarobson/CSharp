45311eeab4bc503d2dfb6d0d62918d8dd67c95b1
Robson Paiva <paivawork@hotmail.com>
2019-12-28 21:50:55 -0300

    Componentes do Form Cadastro de Pessoa renomeados

--- View/CadastroPessoa.Designer.cs
+++ View/CadastroPessoa.Designer.cs
@@ -28,182 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.txbCodigo = new System.Windows.Forms.TextBox();
+            this.lbCodigo = new System.Windows.Forms.Label();
+            this.lbCPF = new System.Windows.Forms.Label();
+            this.lbNome = new System.Windows.Forms.Label();
+            this.lbTipo = new System.Windows.Forms.Label();
+            this.txbCPF = new System.Windows.Forms.TextBox();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.cbTipo = new System.Windows.Forms.ComboBox();
+            this.btNovo = new System.Windows.Forms.Button();
+            this.btAlterar = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.btGravar = new System.Windows.Forms.Button();
+            this.dtGridCadastroPessoa = new System.Windows.Forms.DataGridView();
+            this.errorProviderFrmCadastroPessoa = new System.Windows.Forms.ErrorProvider(this.components);
+            this.toolTipFrmCadastroPessoa = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridCadastroPessoa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderFrmCadastroPessoa)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txbCodigo
             // 
-            this.textBox1.Location = new System.Drawing.Point(20, 91);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(68, 20);
-            this.textBox1.TabIndex = 0;
+            this.txbCodigo.Location = new System.Drawing.Point(20, 91);
+            this.txbCodigo.Name = "txbCodigo";
+            this.txbCodigo.Size = new System.Drawing.Size(68, 20);
+            this.txbCodigo.TabIndex = 0;
             // 
-            // label1
+            // lbCodigo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(17, 72);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Código:";
+            this.lbCodigo.AutoSize = true;
+            this.lbCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCodigo.Location = new System.Drawing.Point(17, 72);
+            this.lbCodigo.Name = "lbCodigo";
+            this.lbCodigo.Size = new System.Drawing.Size(49, 15);
+            this.lbCodigo.TabIndex = 1;
+            this.lbCodigo.Text = "Código:";
             // 
-            // label2
+            // lbCPF
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(91, 73);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "CPF:";
+            this.lbCPF.AutoSize = true;
+            this.lbCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCPF.Location = new System.Drawing.Point(91, 73);
+            this.lbCPF.Name = "lbCPF";
+            this.lbCPF.Size = new System.Drawing.Size(33, 15);
+            this.lbCPF.TabIndex = 2;
+            this.lbCPF.Text = "CPF:";
             // 
-            // label3
+            // lbNome
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(17, 114);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 15);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Nome:";
+            this.lbNome.AutoSize = true;
+            this.lbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNome.Location = new System.Drawing.Point(17, 114);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(44, 15);
+            this.lbNome.TabIndex = 3;
+            this.lbNome.Text = "Nome:";
             // 
-            // label4
+            // lbTipo
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(206, 72);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(34, 15);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Tipo:";
+            this.lbTipo.AutoSize = true;
+            this.lbTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTipo.Location = new System.Drawing.Point(206, 72);
+            this.lbTipo.Name = "lbTipo";
+            this.lbTipo.Size = new System.Drawing.Size(34, 15);
+            this.lbTipo.TabIndex = 4;
+            this.lbTipo.Text = "Tipo:";
             // 
-            // textBox2
+            // txbCPF
             // 
-            this.textBox2.Location = new System.Drawing.Point(94, 91);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(109, 20);
-            this.textBox2.TabIndex = 5;
+            this.txbCPF.Location = new System.Drawing.Point(94, 91);
+            this.txbCPF.Name = "txbCPF";
+            this.txbCPF.Size = new System.Drawing.Size(109, 20);
+            this.txbCPF.TabIndex = 5;
             // 
-            // textBox3
+            // txbNome
             // 
-            this.textBox3.Location = new System.Drawing.Point(20, 132);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(338, 20);
-            this.textBox3.TabIndex = 6;
+            this.txbNome.Location = new System.Drawing.Point(20, 132);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(338, 20);
+            this.txbNome.TabIndex = 6;
             // 
-            // label5
+            // lbTitulo
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(21, 9);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(178, 24);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Cadastro de Pessoa";
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.Location = new System.Drawing.Point(21, 9);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(178, 24);
+            this.lbTitulo.TabIndex = 8;
+            this.lbTitulo.Text = "Cadastro de Pessoa";
             // 
-            // comboBox1
+            // cbTipo
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(209, 90);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(149, 21);
-            this.comboBox1.TabIndex = 9;
+            this.cbTipo.FormattingEnabled = true;
+            this.cbTipo.Location = new System.Drawing.Point(209, 90);
+            this.cbTipo.Name = "cbTipo";
+            this.cbTipo.Size = new System.Drawing.Size(149, 21);
+            this.cbTipo.TabIndex = 9;
             // 
-            // button1
+            // btNovo
             // 
-            this.button1.Location = new System.Drawing.Point(232, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Novo";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btNovo.Location = new System.Drawing.Point(232, 10);
+            this.btNovo.Name = "btNovo";
+            this.btNovo.Size = new System.Drawing.Size(50, 23);
+            this.btNovo.TabIndex = 10;
+            this.btNovo.Text = "Novo";
+            this.btNovo.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btAlterar
             // 
-            this.button2.Location = new System.Drawing.Point(289, 10);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Alterar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btAlterar.Location = new System.Drawing.Point(289, 10);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(50, 23);
+            this.btAlterar.TabIndex = 11;
+            this.btAlterar.Text = "Alterar";
+            this.btAlterar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btCancelar
             // 
-            this.button3.Location = new System.Drawing.Point(375, 130);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "Cancelar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btCancelar.Location = new System.Drawing.Point(375, 130);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btCancelar.TabIndex = 12;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btExcluir
             // 
-            this.button4.Location = new System.Drawing.Point(346, 10);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(50, 23);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "Excluir";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btExcluir.Location = new System.Drawing.Point(346, 10);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(50, 23);
+            this.btExcluir.TabIndex = 13;
+            this.btExcluir.Text = "Excluir";
+            this.btExcluir.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btGravar
             // 
-            this.button5.Location = new System.Drawing.Point(456, 130);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 14;
-            this.button5.Text = "Gravar";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btGravar.Location = new System.Drawing.Point(456, 130);
+            this.btGravar.Name = "btGravar";
+            this.btGravar.Size = new System.Drawing.Size(75, 23);
+            this.btGravar.TabIndex = 14;
+            this.btGravar.Text = "Gravar";
+            this.btGravar.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dtGridCadastroPessoa
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 185);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(519, 178);
-            this.dataGridView1.TabIndex = 15;
+            this.dtGridCadastroPessoa.AllowUserToAddRows = false;
+            this.dtGridCadastroPessoa.AllowUserToDeleteRows = false;
+            this.dtGridCadastroPessoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridCadastroPessoa.Location = new System.Drawing.Point(12, 185);
+            this.dtGridCadastroPessoa.Name = "dtGridCadastroPessoa";
+            this.dtGridCadastroPessoa.ReadOnly = true;
+            this.dtGridCadastroPessoa.Size = new System.Drawing.Size(519, 178);
+            this.dtGridCadastroPessoa.TabIndex = 15;
+            // 
+            // errorProviderFrmCadastroPessoa
+            // 
+            this.errorProviderFrmCadastroPessoa.ContainerControl = this;
             // 
             // frmCadastroPessoa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(549, 378);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dtGridCadastroPessoa);
+            this.Controls.Add(this.btGravar);
+            this.Controls.Add(this.btExcluir);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.btAlterar);
+            this.Controls.Add(this.btNovo);
+            this.Controls.Add(this.cbTipo);
+            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.txbCPF);
+            this.Controls.Add(this.lbTipo);
+            this.Controls.Add(this.lbNome);
+            this.Controls.Add(this.lbCPF);
+            this.Controls.Add(this.lbCodigo);
+            this.Controls.Add(this.txbCodigo);
             this.Name = "frmCadastroPessoa";
             this.Text = "Cadastro Pessoa";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frmCadastroPessoa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridCadastroPessoa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderFrmCadastroPessoa)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -211,20 +221,22 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txbCodigo;
+        private System.Windows.Forms.Label lbCodigo;
+        private System.Windows.Forms.Label lbCPF;
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.Label lbTipo;
+        private System.Windows.Forms.TextBox txbCPF;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.ComboBox cbTipo;
+        private System.Windows.Forms.Button btNovo;
+        private System.Windows.Forms.Button btAlterar;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.Button btGravar;
+        private System.Windows.Forms.DataGridView dtGridCadastroPessoa;
+        private System.Windows.Forms.ErrorProvider errorProviderFrmCadastroPessoa;
+        private System.Windows.Forms.ToolTip toolTipFrmCadastroPessoa;
     }
 }
