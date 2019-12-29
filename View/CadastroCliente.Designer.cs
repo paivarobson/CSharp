7fd707e9ea55b6a46663e4be66f73e908b1da288
Robson Paiva <paivawork@hotmail.com>
2019-12-29 19:24:31 -0300

    CRUD form Cliente Funcional

--- View/CadastroCliente.Designer.cs
+++ View/CadastroCliente.Designer.cs
@@ -33,7 +33,6 @@
             this.lbCodigo = new System.Windows.Forms.Label();
             this.lbCPF = new System.Windows.Forms.Label();
             this.lbNome = new System.Windows.Forms.Label();
-            this.txbCPF = new System.Windows.Forms.TextBox();
             this.txbNome = new System.Windows.Forms.TextBox();
             this.lbTitulo = new System.Windows.Forms.Label();
             this.btNovo = new System.Windows.Forms.Button();
@@ -44,13 +43,16 @@
             this.dtGridCadastroCliente = new System.Windows.Forms.DataGridView();
             this.errorProviderFrmCadastroCliente = new System.Windows.Forms.ErrorProvider(this.components);
             this.toolTipFrmCadastroCliente = new System.Windows.Forms.ToolTip(this.components);
+            this.maskCPF = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridCadastroCliente)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProviderFrmCadastroCliente)).BeginInit();
             this.SuspendLayout();
             // 
             // txbCodigo
             // 
+            this.txbCodigo.Enabled = false;
             this.txbCodigo.Location = new System.Drawing.Point(20, 91);
+            this.txbCodigo.MaxLength = 4;
             this.txbCodigo.Name = "txbCodigo";
             this.txbCodigo.Size = new System.Drawing.Size(68, 20);
             this.txbCodigo.TabIndex = 0;
@@ -86,16 +88,12 @@
             this.lbNome.TabIndex = 3;
             this.lbNome.Text = "Nome:";
             // 
-            // txbCPF
-            // 
-            this.txbCPF.Location = new System.Drawing.Point(94, 91);
-            this.txbCPF.Name = "txbCPF";
-            this.txbCPF.Size = new System.Drawing.Size(109, 20);
-            this.txbCPF.TabIndex = 5;
-            // 
             // txbNome
             // 
+            this.txbNome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txbNome.Enabled = false;
             this.txbNome.Location = new System.Drawing.Point(209, 91);
+            this.txbNome.MaxLength = 50;
             this.txbNome.Name = "txbNome";
             this.txbNome.Size = new System.Drawing.Size(338, 20);
             this.txbNome.TabIndex = 6;
@@ -118,15 +116,18 @@
             this.btNovo.TabIndex = 10;
             this.btNovo.Text = "Novo";
             this.btNovo.UseVisualStyleBackColor = true;
+            this.btNovo.Click += new System.EventHandler(this.AoPressionarNovoRegistro);
             // 
             // btAlterar
             // 
+            this.btAlterar.Enabled = false;
             this.btAlterar.Location = new System.Drawing.Point(391, 9);
             this.btAlterar.Name = "btAlterar";
             this.btAlterar.Size = new System.Drawing.Size(75, 23);
             this.btAlterar.TabIndex = 11;
             this.btAlterar.Text = "Alterar";
             this.btAlterar.UseVisualStyleBackColor = true;
+            this.btAlterar.Click += new System.EventHandler(this.AoPressionarAlterarRegistro);
             // 
             // btCancelar
             // 
@@ -136,18 +137,22 @@
             this.btCancelar.TabIndex = 12;
             this.btCancelar.Text = "Cancelar";
             this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.AoPressionarCancelar);
             // 
             // btExcluir
             // 
+            this.btExcluir.Enabled = false;
             this.btExcluir.Location = new System.Drawing.Point(472, 9);
             this.btExcluir.Name = "btExcluir";
             this.btExcluir.Size = new System.Drawing.Size(75, 23);
             this.btExcluir.TabIndex = 13;
             this.btExcluir.Text = "Excluir";
             this.btExcluir.UseVisualStyleBackColor = true;
+            this.btExcluir.Click += new System.EventHandler(this.AoPressionarExcluirRegistro);
             // 
             // btGravar
             // 
+            this.btGravar.Enabled = false;
             this.btGravar.Location = new System.Drawing.Point(472, 130);
             this.btGravar.Name = "btGravar";
             this.btGravar.Size = new System.Drawing.Size(75, 23);
@@ -160,6 +165,7 @@
             // 
             this.dtGridCadastroCliente.AllowUserToAddRows = false;
             this.dtGridCadastroCliente.AllowUserToDeleteRows = false;
+            this.dtGridCadastroCliente.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dtGridCadastroCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dtGridCadastroCliente.Location = new System.Drawing.Point(12, 159);
             this.dtGridCadastroCliente.Name = "dtGridCadastroCliente";
@@ -171,11 +177,22 @@
             // 
             this.errorProviderFrmCadastroCliente.ContainerControl = this;
             // 
+            // maskCPF
+            // 
+            this.maskCPF.Location = new System.Drawing.Point(95, 91);
+            this.maskCPF.Mask = "000\\.000\\.000\\-00";
+            this.maskCPF.Name = "maskCPF";
+            this.maskCPF.Size = new System.Drawing.Size(108, 20);
+            this.maskCPF.TabIndex = 16;
+            this.maskCPF.Click += new System.EventHandler(this.posicionarCursor);
+            this.maskCPF.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PermitirSomenteNumeros);
+            // 
             // FrmCadastroCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(567, 378);
+            this.Controls.Add(this.maskCPF);
             this.Controls.Add(this.dtGridCadastroCliente);
             this.Controls.Add(this.btGravar);
             this.Controls.Add(this.btExcluir);
@@ -184,7 +201,6 @@
             this.Controls.Add(this.btNovo);
             this.Controls.Add(this.lbTitulo);
             this.Controls.Add(this.txbNome);
-            this.Controls.Add(this.txbCPF);
             this.Controls.Add(this.lbNome);
             this.Controls.Add(this.lbCPF);
             this.Controls.Add(this.lbCodigo);
@@ -205,7 +221,6 @@
         private System.Windows.Forms.Label lbCodigo;
         private System.Windows.Forms.Label lbCPF;
         private System.Windows.Forms.Label lbNome;
-        private System.Windows.Forms.TextBox txbCPF;
         private System.Windows.Forms.TextBox txbNome;
         private System.Windows.Forms.Label lbTitulo;
         private System.Windows.Forms.Button btNovo;
@@ -216,5 +231,6 @@
         private System.Windows.Forms.DataGridView dtGridCadastroCliente;
         private System.Windows.Forms.ErrorProvider errorProviderFrmCadastroCliente;
         private System.Windows.Forms.ToolTip toolTipFrmCadastroCliente;
+        private System.Windows.Forms.MaskedTextBox maskCPF;
     }
 }
--- View/CadastroCliente.cs
+++ View/CadastroCliente.cs
@@ -13,41 +13,234 @@ namespace View
 {
     public partial class FrmCadastroCliente : Form
     {
+        private void FrmCadastroCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+        private enum Status
+        {
+            enInserindo,
+            enEditando,
+            enConsultando
+        }
+
         private bool emInsercao = false;
         private bool emEdicao = false;
+        private bool emConsulta = false;
+
+        ClienteController clienteController = new ClienteController();
         public FrmCadastroCliente()
         {
             InitializeComponent();
 
+            AlterarStatus(Status.enConsultando);
+            HabilitarDesabilitarComponentes();
+
             this.toolTipFrmCadastroCliente.SetToolTip(this.txbCodigo, "Código");
-            this.toolTipFrmCadastroCliente.SetToolTip(this.txbCPF, "Digite o CPF");
+            this.toolTipFrmCadastroCliente.SetToolTip(this.maskCPF, "Digite o CPF");
             this.toolTipFrmCadastroCliente.SetToolTip(this.txbNome, "Digite o nome");
+
         }
 
-        private void FrmCadastroCliente_Load(object sender, EventArgs e)
+        private void AlterarStatus(Status status)
         {
-            
+            emInsercao = false;
+            emEdicao = false;
+            emConsulta = false;
+            switch (status)
+            {
+                case Status.enInserindo:
+                    emInsercao = true;
+                    break;
+                case Status.enEditando:
+                    emEdicao = true;
+                    break;
+                default:
+                    emConsulta = true;
+                    break;
+            }
         }
+
         //CADASTRAR
         private void Cadastrar()
         {
-            this.dtGridCadastroCliente.DataSource = ClienteController.CadastrarCliente(this.txbCPF.Text, this.txbNome.Text);
+            this.dtGridCadastroCliente.DataSource = MessageBox.Show(clienteController.CadastrarCliente(this.maskCPF.Text, this.txbNome.Text));
+
+            Limpar();
+
+            RecuperarUltimoCadastroCliente();
+            Consultar();
+            HabilitarDesabilitarComponentes();
+            maskCPF.Focus();
+            maskCPF.SelectAll();
         }
         //CONSULTAR
         private void Consultar()
         {
-            this.dtGridCadastroCliente.DataSource = ClienteController.ConsultarCliente(Convert.ToInt32(this.txbCodigo.Text));
+            if (!this.txbCodigo.Text.Equals(string.Empty) || 
+                !dtGridCadastroCliente.Rows[0].Cells[0].Value.ToString().Equals(string.Empty))
+            {
+                if (this.txbCodigo.Text.Equals(string.Empty))
+                {
+                    this.txbCodigo.Text = dtGridCadastroCliente.Rows[0].Cells[0].Value.ToString();
+                }
+                //Carregar dados no DataGrid
+                this.dtGridCadastroCliente.DataSource = 
+                    clienteController.ConsultarCliente(Convert.ToInt32(this.txbCodigo.Text));
+
+                if (dtGridCadastroCliente.RowCount > 0)
+                {
+                    this.txbCodigo.Text = dtGridCadastroCliente.Rows[0].Cells[0].Value.ToString();
+                    this.maskCPF.Text = dtGridCadastroCliente.Rows[0].Cells[1].Value.ToString();
+                    this.txbNome.Text = dtGridCadastroCliente.Rows[0].Cells[2].Value.ToString();
+                }
+                else
+                {
+                    maskCPF.Text = string.Empty;
+                    txbNome.Text = string.Empty;
+                    dtGridCadastroCliente.DataSource = null;
+                    MessageBox.Show("Código inválido. Cliente não cadastrado");
+                }
+
+                AlterarStatus(Status.enConsultando);
+                HabilitarDesabilitarComponentes();
+                txbCodigo.Focus();
+                txbCodigo.SelectAll();
+            }
         }
 
+        private void RecuperarUltimoCadastroCliente()
+        {
+            this.dtGridCadastroCliente.DataSource = clienteController.RecuperarUltimoCadastroCliente();
+
+            this.txbCodigo.Text = dtGridCadastroCliente.Rows[0].Cells[0].Value.ToString();
+            this.maskCPF.Text = dtGridCadastroCliente.Rows[0].Cells[1].Value.ToString();
+            this.txbNome.Text = dtGridCadastroCliente.Rows[0].Cells[2].Value.ToString();
+
+        }
+        //ALTERAR
+        private void Alterar()
+        {
+            MessageBox.Show(clienteController.AlterarCliente(
+                Convert.ToInt32(this.txbCodigo.Text), this.maskCPF.Text, this.txbNome.Text));
+
+            Consultar();
+        }
+        //EXCLUIR
+        private string Excluir()
+        {
+            return clienteController.ExcluirCliente(Convert.ToInt32(this.txbCodigo.Text));
+        }
+        //AO PRESSIONAR BOTÃO ALTERAR
+        private void AoPressionarAlterarRegistro(object sender, EventArgs e)
+        {
+            if (!this.txbCodigo.Text.Equals(string.Empty) & !this.maskCPF.Text.Equals(string.Empty))
+            {
+                AlterarStatus(Status.enEditando);
+                HabilitarDesabilitarComponentes();
+                maskCPF.Focus();
+                maskCPF.SelectAll();
+            }
+        }
+        //AO PRESSIONAR BOTÃO NOVO
+        private void AoPressionarNovoRegistro(object sender, EventArgs e)
+        {
+            Limpar();
+            AlterarStatus(Status.enInserindo);
+            HabilitarDesabilitarComponentes();
+            maskCPF.Focus();
+            maskCPF.SelectAll();
+        }
+        //CONTROLE DE HABILITAR/DESABILITAR TODOS OS COMPONENTES DO FORMULÁRIO
+        private void HabilitarDesabilitarComponentes()
+        {
+            //TextBox
+            this.txbCodigo.Enabled = emConsulta;
+            this.maskCPF.Enabled = emInsercao || emEdicao;
+            this.txbNome.Enabled = emInsercao || emEdicao;
+            //Botões
+            this.btNovo.Enabled = emConsulta;
+            this.btAlterar.Enabled = emConsulta &&
+                (!this.txbCodigo.Text.Equals(string.Empty) & !this.maskCPF.Text.Equals(string.Empty));
+            this.btExcluir.Enabled = emConsulta && 
+                (!this.txbCodigo.Text.Equals(string.Empty) & !this.maskCPF.Text.Equals(string.Empty));
+            this.btCancelar.Enabled = emInsercao || emEdicao || emConsulta;
+            this.btGravar.Enabled = emInsercao || emEdicao;
+        }
+        //AO PRESSIONAR BOTÃO GRAVAR
         private void Gravar(object sender, EventArgs e)
         {
-            Cadastrar();
+            maskCPF.TextMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+            if (emInsercao)
+                Cadastrar();
+            else if (emEdicao)
+                Alterar();
+            else
+                Excluir();
         }
 
         private void consultarAoPressionarEnter(object sender, KeyPressEventArgs e)
         {
+            PermitirSomenteNumeros(sender, e);
+
             if (e.KeyChar == 13)
-                Consultar();
+                Consultar(); 
+        }
+
+        private void AoPressionarCancelar(object sender, EventArgs e)
+        {
+            DialogResult confirmacao =
+                MessageBox.Show("Tem certeza que deseja cancelar?", "Cancelar",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+
+            if (confirmacao.ToString().ToUpper() == "YES")
+            {
+                Limpar();
+                AlterarStatus(Status.enConsultando);
+                HabilitarDesabilitarComponentes();
+                txbCodigo.Focus();
+                txbCodigo.SelectAll();
+            }
+
+        }
+
+        private void Limpar()
+        {
+            this.txbCodigo.Text = string.Empty;
+            this.maskCPF.Text = string.Empty;
+            this.txbNome.Text = string.Empty;
+            dtGridCadastroCliente.DataSource = null;
+        }
+
+        private void AoPressionarExcluirRegistro(object sender, EventArgs e)
+        {
+            DialogResult confirmacao =
+                MessageBox.Show("Tem certeza que quer excluir o registro?", "Excluir registro",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+
+            if (confirmacao.ToString().ToUpper() == "YES")
+            {
+                Excluir();
+                Limpar();
+                HabilitarDesabilitarComponentes();
+                txbCodigo.Focus();
+                txbCodigo.SelectAll();
+            }
+            
+        }
+
+        private void PermitirSomenteNumeros(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void posicionarCursor(object sender, EventArgs e)
+        {
+            if (maskCPF.Text.Equals("   .   .   -"))
+                maskCPF.SelectAll();
         }
     }
 }
