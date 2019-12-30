75fd8b7597fca7f221a765ee64b702c58cda7766
Robson Paiva <paivawork@hotmail.com>
2019-12-30 00:09:37 -0300

    Adição da procedure VEICULO

---
+++ scripts/PROCEDURES_VEICULO.sql
@@ -0,0 +1,48 @@
+
+--###  PROCEDURES VEÍCULO ###
+--CADASTRAR 
+CREATE PROC proc_cadastrarVeiculo
+@veiculoPlaca varchar(7),
+@veiculoDescricao varchar(50)
+AS
+INSERT INTO VEICULO (
+	VEIPLC, 
+	VEIDES)
+VALUES (
+	@veiculoPlaca, 
+	@veiculoDescricao)
+GO
+
+--ALTERAR
+CREATE PROC proc_alterarVeiculo
+@veiculoCodigo int,
+@veiculoPlaca varchar(7),
+@veiculoDescricao varchar(50)
+AS
+UPDATE VEICULO 
+SET VEIPLC = @veiculoPlaca,
+	VEIDES = @veiculoDescricao
+WHERE VEICOD = @veiculoCodigo
+GO
+
+--EXCLUIR
+CREATE PROC proc_excluirVeiculo
+@veiculoCodigo int
+AS
+DELETE FROM VEICULO 
+WHERE VEICOD = @veiculoCodigo
+GO
+
+--CONSULTAR
+CREATE PROC proc_consultarVeiculo
+@veiculoCodigo int
+AS
+SELECT * FROM VEICULO
+WHERE VEICOD = @veiculoCodigo
+GO
+
+--RECUPERAR ÚLTIMO REGISTRO CADASTRADO
+CREATE PROC proc_recuperarUltimoCadastroVeiculo
+AS
+SELECT TOP 1 * FROM VEICULO 
+ORDER BY VEICOD DESC
